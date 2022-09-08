40d6f69cf16bad986e689b64b94085f5aab7f4a8
david ramirez <davidramirezrivera2511@gmail.com>
2022-09-08 15:25:38 -0500

    se agrego ES10

---
+++ SRC/ES10/02-try-catch.js
@@ -0,0 +1,12 @@
+try{
+    hello();
+}catch(error){
+    console.log(error);
+}
+
+
+try{
+    anotherFunction();
+} catch{
+    console.log('esto es un error')
+}
---
+++ SRC/ES10/03-fronmEntries.js
@@ -0,0 +1,4 @@
+const entries = new Map ([["name", "alexa"], ["age", 24]]);
+console.log(entries);
+console.log(Object.fromEntries(entries));
+//fromEntries se usa para convertir matrices o vectores en objetos 
